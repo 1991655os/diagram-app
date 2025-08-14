8bd6fcc782dec24088be99fe73673c24592ba168
1991655os <1991655os@gmail.com>
2025-08-14 15:53:43 +0300

    Обновление

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
+// server.js
 const express = require('express');
 const path = require('path');
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
-// Раздаём все файлы из папки проекта
+// Отдаём статические файлы
 app.use(express.static(path.join(__dirname)));
 
-// Отдаём diagram.html при заходе на сайт
+// Главная страница
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'diagram.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`Сервер запущен на порту ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });

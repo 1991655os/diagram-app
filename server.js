5b75e6d7f041f38b2ec1c6443a6f3efa29ed8316
1991655os <1991655os@gmail.com>
2025-08-14 16:01:01 +0300

    Fix port for Render

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'diagram.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
 });
